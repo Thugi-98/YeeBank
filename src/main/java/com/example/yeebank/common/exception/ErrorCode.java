0e88ca47d0a1b038482688c94f94409a887f2280
shinhojun <hosujjing@gmail.com>
2026-01-06 20:32:07 +0900

    ✨ feat: 공통응답 객체 생성, 커스텀예외 생성
    공통응답 객체 생성,GlovalException , dto

--- src/main/java/com/example/yeebank/common/exception/ErrorCode.java
+++ src/main/java/com/example/yeebank/common/exception/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+
     // Valid 검증 ErrorCode
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다: "),
 
@@ -25,7 +26,7 @@ public enum ErrorCode {
 
     // 거래 관련 ErrorCode
     TRANSFER_NOT_FOUND_SEND(HttpStatus.NOT_FOUND, "존재하지 않는 계좌로는 송금을 할 수 없습니다."),
-//    TRANSFER_AMOUNT_MISS(HttpStatus.), 보유 잔액이 부족할때
+//    TRANSFER_AMOUNT_MISS(HttpStatus.N , ""), //보유 잔액이 부족할때
     TRANSFER_CLIENT_MISS(HttpStatus.NOT_FOUND, "요청 클라이언트를 찾을 수 없습니다.");
 
     private final HttpStatus status;
