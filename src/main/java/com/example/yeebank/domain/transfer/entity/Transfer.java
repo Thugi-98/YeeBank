4d55e76fafd8bbc516b8cf37eddd7443838e07be
Michael-930813 <michael.930813@gmail.com>
2026-01-07 10:57:12 +0900

    ✨ feat: Transfer 기본 구조 설계
    1. 엔티티 설계
    2. MVC구조 설계

--- src/main/java/com/example/yeebank/domain/transfer/entity/Transfer.java
+++ src/main/java/com/example/yeebank/domain/transfer/entity/Transfer.java
@@ -1,32 +1,48 @@
 package com.example.yeebank.domain.transfer.entity;
 
 import com.example.yeebank.common.entity.BaseEntity;
+import com.example.yeebank.domain.transfer.enums.TransferStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "transfers")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@NoArgsConstructor
 public class Transfer extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+// - Properties
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
+    @Column(name = "from_account_id")
+    private Long fromAccountId;
+    @Column(name = "to_account_id")
+    private Long toAccountId;
+    @Column(name = "amount", nullable = false)
     private Long amount;
+    @Column(name = "status", nullable = false)
+    private TransferStatus status;
+    @Column(name = "fail_reason", length = 200)
+    private String failReason;
+    @Column(name = "request_client_id")
+    private Long requestClientId;
+    @Column(name = "create_at") @CreatedDate
+    private LocalDateTime craetedAt;
 
-    @Column(length = 500, nullable = false)
-    private String status;
-
-    @Column(length = 200)
-    private String fail_reason;
-
-    @Column
-    private Long request_client_id;
+// - Methods
+    public Transfer(Long fromAccountId, Long toAccountId, Long amount, TransferStatus status, String failReason, Long requestClientId) {
+        this.fromAccountId = fromAccountId;
+        this.toAccountId = toAccountId;
+        this.amount = amount;
+        this.status = status;
+        this.failReason = failReason;
+        this.requestClientId = requestClientId;
+    }
 }
