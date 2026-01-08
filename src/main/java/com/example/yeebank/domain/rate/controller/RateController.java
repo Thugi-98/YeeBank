1c4c9ba308a4352f83a27456da3aa2385ec56878
Thugi-98 <kimjh4533@gmail.com>
2026-01-09 06:49:12 +0900

    ✨ feat: 환율 조회 기능 개선
    - 환율 조회 시 API 값 전체를 불러오지 않고 USDKRW 값만 불러올 수 있도록 구현

--- src/main/java/com/example/yeebank/domain/rate/controller/RateController.java
+++ src/main/java/com/example/yeebank/domain/rate/controller/RateController.java
@@ -1,10 +1,14 @@
 package com.example.yeebank.domain.rate.controller;
 
+import com.example.yeebank.common.dto.CommonResponse;
 import com.example.yeebank.domain.rate.service.RateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/rates")
@@ -14,9 +18,14 @@ public class RateController {
     private final RateService rateService;
 
     @GetMapping
-    public String getRatesApi() {
-        String result = rateService.getRequest();
+    public CommonResponse<Map<String, BigDecimal>> getRatesApi() {
 
-        return result;
+        BigDecimal rate = rateService.getRates();
+
+        Map<String, BigDecimal> data = Map.of(
+                "USDKRW", rate
+        );
+
+        return new CommonResponse<>(true, "환율 조회 성공", data);
     }
 }
--- src/main/java/com/example/yeebank/domain/rate/service/RateService.java
+++ src/main/java/com/example/yeebank/domain/rate/service/RateService.java
@@ -1,10 +1,13 @@
 package com.example.yeebank.domain.rate.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -13,15 +16,22 @@ public class RateService {
     private final RestTemplate restTemplate;
 
     @Cacheable(value = "rateCache")
-    public String getRequest() {
-        String apiUrl = "https://api.exchangerate.host/live?access_key=eed12fb49aea8aec09a8eb53396f4d6b&currencies=USD,KRW&format=1";
+    public BigDecimal getRates() {
 
-        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+        String key = "eed12fb49aea8aec09a8eb53396f4d6b";
+        String url = String.format("https://api.exchangerate.host/live?access_key={}&currencies=USD,KRW&format=1", key);
 
-        String responseBody = response.getBody();
+        String response = restTemplate.getForObject(url, String.class);
 
-        return responseBody;
+        try {
+            JsonNode node = new ObjectMapper().readTree(response)
+                    .path("quotes")
+                    .path("USDKRW");
 
+            return node.decimalValue();
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("환율 정보 파싱 실패");
+        }
     }
-
 }
