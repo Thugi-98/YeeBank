a6237189fba88ef30c8b320385618e5f513cebe3
shinhojun <hosujjing@gmail.com>
2026-01-08 22:20:16 +0900

    🐛 fix: 리팩토링
    리팩토링

--- src/main/java/com/example/yeebank/common/exception/ErrorCode.java
+++ src/main/java/com/example/yeebank/common/exception/ErrorCode.java
@@ -38,7 +38,6 @@ public enum ErrorCode {
 
     // 거래 관련 ErrorCode
     TRANSFER_NOT_FOUND_SEND(HttpStatus.NOT_FOUND, "존재하지 않는 계좌로는 송금을 할 수 없습니다."),
-//    TRANSFER_AMOUNT_MISS(HttpStatus.N , ""), //보유 잔액이 부족할때
     TRANSFER_CLIENT_MISS(HttpStatus.NOT_FOUND, "요청 클라이언트를 찾을 수 없습니다."),
 
     // Lock 관련 ErrorCode
